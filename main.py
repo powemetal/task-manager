f5d2989bea06ed4ef1c4bb6648e2aa238761c411
Francis Boisvert <francisbvrt@gmail.com>
2025-12-11 10:24:06 -0500

    feat: add remove_task method
    Closes #1

--- main.py
+++ main.py
@@ -11,7 +11,16 @@ manager.add_task("Réviser pour l'exam")
 
 # Compléter une tâche
 manager.complete_task(0)
+manager.complete_task(1)
 
 # Afficher
 print("=== Mes tâches ===")
 manager.display_all()
+
+#Supprimer une tache
+def remove_task(self, index):
+    """Supprime une tâche par son index."""
+    if 0 <= index < len(self.tasks):
+        del self.tasks[index]
+        return True
+    return False
